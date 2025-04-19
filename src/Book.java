a4ebcd02db312568ef8f18b888a09a5526608c8c
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-19 13:01:32 -0700

    update

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Book {
+    private String title = "hello";
+    private String author;
+    private int copies;
+
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public int getCopies(){
+        return  copies;
+    }
+
+    public void setString(String title, String author, int copies){
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+    }
+
+
+    Book(){
+        title = "Habits";
+        author = "Duke";
+        copies = 10;
+    }
+
+    Book(String titre, String auteur, int nombres){
+        title = titre;
+        author = auteur;
+        copies = nombres;
+    }
+
+    public void show(){
+        System.out.printf("""
+                    Title is: %s
+                    Author is: %s
+                    Copies are: %d
+                    """, title,author,copies);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Book b1 = new Book();
+        b1.setString("Atomicccc", "Duke", 10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Book b2 = new Book("Java", "Dave", 25); // HERE WE ARE NOT USING GETTER AND SETTER JUST THE CONSTRUCTOR; = "Python";
+
+        b1.show();
+        System.out.println("You got the book: " + b1.getTitle());
+        System.out.println("The author of book 1 is: " + b1.getAuthor());
+        System.out.println("The number of book 1 copies are: " + b1.getCopies());
+
+        b2.show();
+
+
+        // Create a Person object
+        Person person = new Person();
+
+        // Use setters to set field values
+        person.setName("Alice");
+        person.setAge(25);
+
+        // Use getters to access field values
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+
+        // Try to set an invalid age
+        person.setAge(-5); // Output: Age must be positive.
+
+        Greeter worldGreeting = new Greeter();
+        System.out.println("" + worldGreeting.getGreeting());
+
     }
+}
+
+class Greeter {
+
+    String getGreeting() {
+        return "Hello, World!";
+    }
+
 }
